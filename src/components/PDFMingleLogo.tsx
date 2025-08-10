1c3c205634eee6675abc4d40aedb3c799884c4e9
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-10 22:21:03 +0530

    Update PDFMingleLogo.tsx

--- src/components/PDFMingleLogo.tsx
+++ src/components/PDFMingleLogo.tsx
@@ -1,1 +1,7 @@
+import { Link } from "react-router-dom";
 
+export const PDFMingleLogo = () => (
+  <Link to="/" className="flex items-center text-2xl font-bold tracking-tighter text-ilovepdf-text no-underline">
+    <span className="text-ilovepdf-red">PDF</span>Mingle
+  </Link>
+);
