a3dc1eb5eb79d566c93e3f173e2e8842b08e8bf0
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-11 00:18:15 +0530

    Update DesktopNav.tsx

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -77,7 +77,7 @@ const ListItem = React.forwardRef<
       <NavigationMenuLink asChild>
         <Link
           ref={ref}
-          to={to}
+          to={to ?? "/"}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
             className
