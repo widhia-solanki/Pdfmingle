91c3578ea4434e9db09d07984cc39e2f48ca3b1d
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-14 23:52:18 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { MainLayout } from "@/layouts/MainLayout";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MainLayout } from '@/layouts/MainLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
