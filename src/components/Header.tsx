2add76bb7f5ab5e95cb58bc5fa7900b770980b7c
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-14 21:47:06 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,17 +17,8 @@ export const Header = () => {
           )}
           <PDFMingleLogo />
         </div>
-        {/* The <ToolsMenu /> component has been removed from here */}
+        {/* The ToolsMenu component has been removed */}
       </div>
     </header>
   );
-};```
-
-### Summary of the Changes:
-
-1.  **Removed Import:** The line `import { ToolsMenu } from "./ToolsMenu";` has been deleted.
-2.  **Removed Component:** The `<ToolsMenu />` component at the end of the header layout has been deleted.
-
-After you commit this change, the header will be updated, and the grid menu icon on the right side will be completely gone.
-
-**Optional Cleanup:** For a perfectly clean project, you can now also safely **delete** the file `src/components/ToolsMenu.tsx`, as it is no longer being used anywhere.
+};
