a1fd17ee53e43b6b12fd8e7b923670717bcbf817
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-15 20:09:48 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { MainLayout } from '@/layouts/MainLayout'; // CORRECT IMPORT: With curly braces
+import { MainLayout } from '@/layouts/MainLayout';
+import { SpeedInsights } from "@vercel/speed-insights/next"; // 1. IMPORT SpeedInsights
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MainLayout>
       <Component {...pageProps} />
+      <SpeedInsights /> 
+      {/* 2. ADD the component here */}
     </MainLayout>
   );
 }
