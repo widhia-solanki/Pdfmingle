a9bcebb2a87d0800b84a6c9e6e8499eea4c4debd
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-16 19:54:06 +0530

    Update ToolGrid.tsx

--- src/components/ToolGrid.tsx
+++ src/components/ToolGrid.tsx
@@ -1,14 +1,17 @@
 import Link from 'next/link';
 import { Tool } from '@/constants/tools';
 
+// The component now only needs to know which tools to display
 interface ToolGridProps {
   tools: Tool[];
 }
 
 export const ToolGrid = ({ tools }: ToolGridProps) => {
   return (
+    // This section has a light gray background to separate it from the footer
     <section className="w-full bg-gray-50 py-16 md:py-24">
       <div className="container mx-auto px-4">
+        {/* The grid of tool cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {tools.map((tool) => {
             const Icon = tool.icon;
