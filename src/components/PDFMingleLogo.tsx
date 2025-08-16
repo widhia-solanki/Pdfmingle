18e5f36f42b223cbe25e05c65d54958a82d8b009
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-16 17:29:37 +0530

    Update PDFMingleLogo.tsx

--- src/components/PDFMingleLogo.tsx
+++ src/components/PDFMingleLogo.tsx
@@ -1,11 +1,11 @@
-// src/components/PDFMingleLogo.tsx
-import Image from 'next/image'
-import Link from "next/link";
+import Image from "next/link";
 
 export const PDFMingleLogo = () => (
-    // We replace Link
-    <Link href="/" className="flex items-center gap-3 text-2xl font-bold tracking-tighter text-ilovepdf-text no-underline">
-        <Image src="/pdfmingle_logo.png" width={48} height={48} alt="PDFMingle Logo" priority />
-        <span>PDFMingle</span>
-    </Link>
+<a href="/">
+
+   {/*TESTING - hardcoded Image*/}
+   <img src="/pdfmingle_logo.png" width="48" height="48" alt="PDFMingle Logo"/>
+
+<span>PDFMingle</span>
+ </a>
 );
