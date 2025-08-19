8696b61ce1547b982971f53b32476463b7c9ded8
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-19 20:22:40 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,5 @@
 // next-sitemap.config.js
 
-// Import the list of your tools to make the sitemap dynamic
-const { tools } = require('./src/constants/tools.js');
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://pdfmingle.net',
@@ -10,13 +7,21 @@ module.exports = {
   
   // Custom transformation to add priority and changefreq
   transform: async (config, path) => {
-    let priority = 0.7; // Default priority
+    let priority = 0.7; // Default priority for pages like about, contact, etc.
     
+    // Manually check if the path is a tool page
+    const toolPages = [
+      '/merge-pdf', '/split-pdf', '/compress-pdf', '/pdf-to-word', '/word-to-pdf',
+      '/pdf-to-excel', '/excel-to-pdf', '/pdf-to-ppt', '/ppt-to-pdf', '/image-to-pdf',
+      '/pdf-to-image', '/protect-pdf', '/unlock-pdf', '/rotate-pdf', '/esign-pdf',
+      '/edit-pdf', '/organize-pdf'
+    ];
+
     if (path === '/') {
       priority = 1.0;
-    } else if (tools.some(tool => `/${tool.value}` === path)) {
+    } else if (toolPages.includes(path)) {
       priority = 0.9;
-    } else if (path.startsWith('/blog/')) {
+    } else if (path.startsWith('/blog')) {
       priority = 0.8;
     }
     
@@ -28,8 +33,8 @@ module.exports = {
     };
   },
 
-  // Optional: Add any pages to exclude from the sitemap
-  // exclude: ['/secret-page'],
+  // Add any pages to exclude from the sitemap
+  exclude: [],
 
   robotsTxtOptions: {
     policies: [
@@ -38,28 +43,5 @@ module.exports = {
         allow: '/',
       },
     ],
-    // The sitemap URL will be automatically added, but you can add more here if needed
-    // additionalSitemaps: [
-    //   'https://pdfmingle.net/server-sitemap.xml',
-    // ],
   },
 };
-```*Note: I had to change this file to `.js` and use `require` because your project doesn't have ES modules configured for Node.js scripts by default. This is the standard approach.*
-
-### **Step 3: Update the `build` script in `package.json`**
-
-Finally, we need to tell Vercel to run the sitemap generator **after** the build is complete.
-
-1.  Navigate back to your `package.json` file and click **Edit**.
-2.  Add a new script called `postbuild`. This special script automatically runs after the `build` script finishes.
-
-Your `"scripts"` section should look like this:
-
-```json
-  "scripts": {
-    "dev": "next dev",
-    "build": "next build",
-    "postbuild": "next-sitemap",
-    "start": "next start",
-    "lint": "next lint"
-  },
