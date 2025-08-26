848fc6d2fec1b5518ef357dc0c611db594d52826
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-26 19:22:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: "javascript/auto",
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
