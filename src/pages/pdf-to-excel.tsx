855a754af02c1687f5fc2434f8cb34790888f0cb
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-27 19:21:52 +0530

    Create pdf-to-excel.tsx

---
+++ src/pages/pdf-to-excel.tsx
@@ -0,0 +1,17 @@
+import { ToolComingSoon } from "@/components/ToolComingSoon";
+import { TOOLS } from "@/constants/tools";
+import { NextSeo } from "next-seo";
+
+const PdfToExcelPage = () => {
+    const tool = TOOLS.find(t => t.key === 'pdf-to-excel');
+
+    if (!tool) return <div>Tool not found</div>;
+
+    return (
+        <>
+            <NextSeo title={tool.title} description={tool.description} />
+            <ToolComingSoon tool={tool} />
+        </>
+    )
+}
+export default PdfToExcelPage;
