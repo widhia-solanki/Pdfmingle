a7a8e68453fd56a633aa0ba171770b61f13d7d17
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-30 16:14:10 +0530

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,14 +9,17 @@ interface MainLayoutProps {
 }
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+ <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-dark-bg">
+      <CustomCursor />
       <Header />
-      <main className="flex-grow">
+      {/* --- THIS IS THE FIX --- */}
+      {/* Added pt-20 to offset the sticky h-20 header */}
+      <main className="flex-grow pt-20">
         {children}
       </main>
       
       {/* The footer is now handled by the InformativePanel on the homepage */}
+      <InformativePanel />
       
       <FeedbackButton />
     </div>
