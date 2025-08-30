bc7df0f92f31f376b99a8fe2936f84ee2e64d7ba
Eshan Solanki <shamoonbai25@gmail.com>
2025-08-30 16:16:42 +0530

    Update MainLayout.tsx

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,24 +1,25 @@
+// src/layouts/MainLayout.tsx
+
 import React from "react";
 import { Header } from "@/components/Header";
-import { cn } from "@/lib/utils";
 import { FeedbackButton } from "@/components/FeedbackButton";
-import { CustomCursor } from "@/components/CustomCursor"; // <-- Import the new component
+// --- THIS IS THE FIX ---
+import { InformativePanel } from "@/components/InformativePanel"; 
+import { CustomCursor } from "@/components/CustomCursor";
 
 interface MainLayoutProps {
   children: React.ReactNode;
 }
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
- <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-dark-bg">
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-dark-bg">
       <CustomCursor />
       <Header />
-      {/* --- THIS IS THE FIX --- */}
-      {/* Added pt-20 to offset the sticky h-20 header */}
-      <main className="flex-grow pt-20">
+      <main className="flex-grow">
         {children}
       </main>
       
-      {/* The footer is now handled by the InformativePanel on the homepage */}
       <InformativePanel />
       
       <FeedbackButton />
