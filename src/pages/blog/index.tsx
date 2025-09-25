e365c618c1f6a353f4e55f6ece90ecc783635a04
Eshan Solanki <shamoonbai25@gmail.com>
2025-09-25 13:42:11 +0530

    Update index.tsx

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -99,4 +99,4 @@ export const getStaticProps: GetStaticProps = async () => {
   };
 };
 
-export default BlogIndexPage; compatible with your site's dark mode, creating a seamless experience for your readers.
+export default BlogIndexPage;
