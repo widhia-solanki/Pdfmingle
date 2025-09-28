6ba1c73e1283c58f184337e9bc2cd953bcb84efd
Eshan Solanki <shamoonbai25@gmail.com>
2025-09-28 22:08:49 +0530

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,9 @@
+// src/lib/firebase.ts
+
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // Import Firestore
+
+const firebaseConfig = {
+  // ... your existing config
+};
