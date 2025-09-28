e345f2c770893bd89a54dab17574b4701a2d994d
Eshan Solanki <shamoonbai25@gmail.com>
2025-09-28 20:33:33 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,24 +11,22 @@ import SEO from '../../next-seo.config';
 import { CookieConsent } from '@/components/CookieConsent';
 import { ThemeProvider } from 'next-themes';
 import { useRouter } from 'next/router';
-import { SessionProvider } from "next-auth/react"; // Import the new provider
+import { SessionProvider } from "next-auth/react";
 
-// The AppProps type needs to be updated to include the session
 interface AppPropsWithSession extends AppProps {
   pageProps: {
-    session: any; // Using `any` for simplicity, can be typed more strictly if needed
+    session: any;
     [key: string]: any;
   };
 }
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppPropsWithSession) {
   const router = useRouter();
-  const flushLayoutRoutes = new Set(['/add-watermark', '/edit-pdf']);
-  const isHomePage = router.pathname === '/';
-  const shouldUseFlushLayout = isHomePage || flushLayoutRoutes.has(router.pathname);
+  
+  const flushLayoutRoutes = new Set(['/', '/add-watermark', '/edit-pdf']);
+  const shouldUseFlushLayout = flushLayoutRoutes.has(router.pathname);
 
   return (
-    // The SessionProvider must be the outermost provider to work correctly
     <SessionProvider session={session}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Head>
