dde02be2e63b6dde403c11250fc73fcbbd7f494b
Eshan Solanki <shamoonbai25@gmail.com>
2025-09-28 18:37:58 +0530

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,7 +10,6 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          // This is how we request the Google Drive scope
           scope: "openid email profile https://www.googleapis.com/auth/drive.file",
         },
       },
@@ -19,5 +18,28 @@ export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
+  },
+  // --- THIS IS THE FIX ---
+  // The callbacks block allows us to control the content of the JWT and session.
+  callbacks: {
+    async jwt({ token, user, account }) {
+      // On the initial sign-in, the `user` and `account` objects are available.
+      if (account && user) {
+        return {
+          ...token,
+          // Persist the user's name and picture to the JWT
+          name: user.name,
+          picture: user.image,
+        };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // The session callback receives the JWT's content in the `token` object.
+      // We add the custom properties to the session object here.
+      session.user.name = token.name as string;
+      session.user.image = token.picture as string;
+      return session;
+    },
   },
 });
