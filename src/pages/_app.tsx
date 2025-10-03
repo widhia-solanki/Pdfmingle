da638d76065017f7c83f349a0546ddd54dd402d2
Eshan Solanki <shamoonbai25@gmail.com>
2025-10-03 14:03:59 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ import { CookieConsent } from '@/components/CookieConsent';
 import { ThemeProvider } from 'next-themes';
 import { useRouter } from 'next/router';
 import { SessionProvider } from "next-auth/react";
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 interface AppPropsWithSession extends AppProps {
   pageProps: {
@@ -22,25 +23,32 @@ interface AppPropsWithSession extends AppProps {
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppPropsWithSession) {
   const router = useRouter();
-  
   const flushLayoutRoutes = new Set(['/', '/add-watermark', '/edit-pdf']);
   const shouldUseFlushLayout = flushLayoutRoutes.has(router.pathname);
+  const recaptchaSiteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
+
+  if (!recaptchaSiteKey) {
+    // This provides a clear error if the key is missing
+    return <div>Error: reCAPTCHA Site Key is not configured.</div>;
+  }
 
   return (
-    <SessionProvider session={session}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="google-adsense-account" content="ca-pub-9837860640878429" />
-        </Head>
-        <DefaultSeo {...SEO} />
-        <MainLayout flush={shouldUseFlushLayout}>
-          <Component {...pageProps} />
-        </MainLayout>
-        <SpeedInsights />
-        <Analytics />
-        <CookieConsent />
-      </ThemeProvider>
-    </SessionProvider>
+    <GoogleReCaptchaProvider reCaptchaKey={recaptchaSiteKey}>
+      <SessionProvider session={session}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="google-adsense-account" content="ca-pub-9837860640878429" />
+          </Head>
+          <DefaultSeo {...SEO} />
+          <MainLayout flush={shouldUseFlushLayout}>
+            <Component {...pageProps} />
+          </MainLayout>
+          <SpeedInsights />
+          <Analytics />
+          <CookieConsent />
+        </ThemeProvider>
+      </SessionProvider>
+    </GoogleReCaptchaProvider>
   );
 }
